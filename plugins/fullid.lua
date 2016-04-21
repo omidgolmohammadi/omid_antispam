f9aaf4a316b412296dab72dbca2ef3d3baad9925
Arash alone boy <titanteam0098@gmail.com>
2016-04-21 12:56:52 +0430

    Update fullid.lua

--- plugins/fullid.lua
+++ plugins/fullid.lua
@@ -1,19 +1,19 @@
 do function run(msg, matches)
 
 	  if matches[1]:lower() == 'id' and is_sudo(msg) then
-      return "Sudo☑\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Your name⭕️\n"..msg.from.print_name.."\n⭕️Group name⭕️\n"..msg.to.title.."\n⭕️Group id⭕️\n"..msg.to.id
+      return "سودو☑\n⭕️آیدی شما⭕️\n"..msg.from.id.."\n⭕️نام شما⭕️\n"..msg.from.print_name.."\n⭕️نام گروه⭕️\n"..msg.to.title.."\n⭕️آیدی گروه⭕️\n"..msg.to.id
       end
       if matches[1]:lower() == 'id' and is_admin(msg) then
-      return "Admin☑️\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Your name⭕️\n"..msg.from.print_name.."\n⭕️Group name⭕️\n"..msg.to.title.."\n⭕️Group id⭕️\n"..msg.to.id
+      return "ادمین☑️\n⭕️آیدی شما⭕️\n"..msg.from.id.."\n⭕️نام شما⭕️\n"..msg.from.print_name.."\n⭕️نام گروه⭕️\n"..msg.to.title.."\n⭕️آیدی گروه⭕️\n"..msg.to.id
       end
       if matches[1]:lower() == 'id' and is_owner(msg) then
-      return "Owner☑️\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Your name⭕️\n"..msg.from.print_name.."\n⭕️Group name⭕️\n"..msg.to.title.."\n⭕️Group id⭕️\n"..msg.to.id
+      return "صاحب گروه☑️\n⭕️آیدی شما⭕️\n"..msg.from.id.."\n⭕️نام شما⭕️\n"..msg.from.print_name.."\n⭕️نام گروه⭕️\n"..msg.to.title.."\n⭕️آیدی گروه⭕️\n"..msg.to.id
       end
       if matches[1]:lower() == 'id' and is_momod(msg) then
-      return "Moderator☑️\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Your name⭕️\n"..msg.from.print_name.."\n⭕️Group name⭕️\n"..msg.to.title.."\n⭕️Group id⭕️\n"..msg.to.id
+      return "مود☑️\n⭕️آیدی شما⭕️\n"..msg.from.id.."\n⭕️نام شما⭕️\n"..msg.from.print_name.."\n⭕️نام گروه⭕️\n"..msg.to.title.."\n⭕️آیدی گروه⭕️\n"..msg.to.id
       end
       if matches[1]:lower() == 'id' then
-      return "Member☑️\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Your name⭕️\n"..msg.from.print_name.."\n⭕️Group name⭕️\n"..msg.to.title.."\n⭕️Group id⭕️\n"..msg.to.id
+      return "کاربر معمولی☑️\n⭕️آیدی شما⭕️\n"..msg.from.id.."\n⭕️نام شما⭕️\n"..msg.from.print_name.."\n⭕️نام گروه⭕️\n"..msg.to.title.."\n⭕️آیدی گروه⭕️\n"..msg.to.id
     end
 end
   return {
@@ -25,5 +25,3 @@ end
   run = run
 }
 end
-
---sepehr blacklifeTM
